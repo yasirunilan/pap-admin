ae0d0f607c5ba62e56332e5143719a5baf5912b4
yasirunilan <nilanyasiru@gmail.com>
2021-07-31 14:23:40 +0530

    first commit

---
+++ src/PapAdmin.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+function PapAdmin() {
+    return (
+        <SignIn />
+        // <SignUp />
+    );
+}
+
+export default PapAdmin;
